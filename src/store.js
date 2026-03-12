ad3023515b2560c473772baa8e6aec874ae28968
ThanhsonWeb <sondeptroi@gmail.com>
2026-03-13 05:36:49 +0700

    Create a redux store

---
+++ src/store.js
@@ -0,0 +1,69 @@
+import { createStore } from "redux";
+
+const initialState = {
+	balance: 0,
+	loan: 0,
+	loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+	switch (action.type) {
+		case "account/deposit":
+			return { ...state, balance: state.balance + action.payload };
+		case "account/withdraw":
+			return { ...state, balance: state.balance - action.payload };
+		case "account/requestLoan":
+			if (state.loan > 0) return state;
+			// Later
+			return {
+				...state,
+				loan: action.payload.amount,
+				loanPurpose: action.payload.purpose,
+			};
+		case "account/payLoan":
+			return {
+				...state,
+				loan: 0,
+				loanPurpose: "",
+				balance: state.balance - state.loan,
+			};
+		default:
+			return state;
+	}
+}
+// Redux here
+const store = createStore(reducer);
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// console.log(store.getState());
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// console.log(store.getState());
+
+// store.dispatch({
+// 	type: "account/requestLoan",
+// 	payload: { amount: 1000, purpose: "Buy a car" },
+// });
+// console.log(store.getState());
+
+// store is where you store state ! and we give info to the store to update
+
+// create 4 function !
+
+function deposit(amount) {
+	return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+	return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+	return { type: "account/requestLoan", payload: { amount, purpose } };
+}
+function payLoan() {
+	return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(1000, "Buy a car"));
+store.dispatch(payLoan());
+console.log(store.getState());
